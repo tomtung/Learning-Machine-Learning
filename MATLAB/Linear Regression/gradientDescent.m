bf1c710f6a748beb36796c2fdb851d0cbdac8fb0
Tom Tung <tom.tung.dyb@gmail.com>
2011-10-22 21:34:28 +0800

    In gradient descent of linear regression: re-write it in matrix-vectorial notation

--- MATLAB/Linear Regression/gradientDescent.m
+++ MATLAB/Linear Regression/gradientDescent.m
@@ -8,13 +8,8 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
-
-    e = zeros(m, 1); % error of each traning data
-    for i = 1:m
-        e(i) = X(i,:) * theta - y(i);
-    end
     
-    gradJ = X'* e / m;
+    gradJ = X'* (X*theta - y) / m;
     theta = theta - alpha * gradJ;
     
     % Save the cost J in every iteration    
